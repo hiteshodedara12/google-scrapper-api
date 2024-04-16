7d0439b5317c9d98273fe4b4100667ab1ca16925
Odedara Hitesh <hiteshodedara1010@gmail.com>
2024-04-16 13:00:03 +0530

    now we can pass paramiters like apiKey,platform,country,query

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const https = require('https'); // Import the https module
+const https = require('https');
 
 const app = express();
 const PORT = 3000;
@@ -12,11 +12,15 @@ app.use(cors());
 // Middleware to fetch data from another API
 const fetchDataMiddleware = async (req, res, next) => {
     try {
-        const response = await axios.get('https://scrapper.stylekaro.in/google-shopping/?apiKey=abcdefghijk&platform=google&country=in&query=men-tshirts', {
-            httpsAgent: new https.Agent({ rejectUnauthorized: false }) // Bypass SSL verification
+        const { apiKey, platform, country, query } = req.query; // Extract query parameters from the request
+        const apiUrl = `https://scrapper.stylekaro.in/google-shopping/?apiKey=${apiKey}&platform=${platform}&country=${country}&query=${query}`;
+
+        const response = await axios.get(apiUrl, {
+            httpsAgent: new https.Agent({ rejectUnauthorized: false })
         });
+
         req.products = response.data; // Assign fetched data to request object
-        next(); // Call next middleware
+        next(); // Call next middleware 
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Internal Server Error' });
